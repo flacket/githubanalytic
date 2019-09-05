08fe763029c217fd996aa8289ca0b15134e09b92
Facundo Gallardo <facundoggallardo@gmail.com>
2019-09-05 15:24:08 -0300

    Update FirebaseApp.js
    borrado información personal de la configuración de firebase

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -3,13 +3,7 @@ import 'firebase/auth'
 
 //Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCzrBIkMUFCRNS54cNNcJKm-GJUbXfQ8Sk",
-  authDomain: "github-analytic.firebaseapp.com",
-  databaseURL: "https://github-analytic.firebaseio.com",
-  projectId: "github-analytic",
-  storageBucket: "",
-  messagingSenderId: "13664469414",
-  appId: "1:13664469414:web:61898e0bfdc563ecb28f67"
+  //your config here
 }
 
 // Initialize Firebase
