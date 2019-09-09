e2a3403fedbd3250ff89fda0c3a5348991372354
Facundo Gallardo <facundoggallardo@gmail.com>
2019-09-09 18:43:43 -0300

    Set Environment Variables

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -2,7 +2,13 @@ import * as firebase from "firebase/app"
 import 'firebase/auth'
 
 const firebaseConfig = {
-  //your config here
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID
 }
 
 // Initialize Firebase
