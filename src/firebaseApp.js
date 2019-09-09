4b39d3739475748f50420b0ddee30128e826d30b
Facundo Gallardo <facundoggallardo@gmail.com>
2019-09-09 18:26:19 -0300

    Remove firebase config

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,15 +1,9 @@
 import * as firebase from "firebase/app"
 import 'firebase/auth'
 
-//Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCzrBIkMUFCRNS54cNNcJKm-GJUbXfQ8Sk",
-  authDomain: "github-analytic.firebaseapp.com",
-  databaseURL: "https://github-analytic.firebaseio.com",
-  projectId: "github-analytic",
-  storageBucket: "",
-  messagingSenderId: "13664469414",
-  appId: "1:13664469414:web:61898e0bfdc563ecb28f67"
+  //Your web app's Firebase configuration
 }
 
 // Initialize Firebase
