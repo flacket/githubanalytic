c383c615da4292a9ee469ae468d7231bb760fcdf
Facundo Gallardo <facundoggallardo@gmail.com>
2019-09-09 20:08:02 -0300

    Update Firebase Config

--- src/FirebaseApp.js
+++ src/FirebaseApp.js
@@ -5,8 +5,7 @@ const firebaseConfig = {
 apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
 authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
 databaseURL: 'https://' + process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseio.com',
-projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.appspot.com'
+projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
 }
 
 // Initialize Firebase
