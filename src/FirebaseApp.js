7fb9c17153d3349e5e43ef60308049a7c10e875c
flacket <facundoggallardo@gmail.com>
2019-09-10 01:20:09 -0300

    Update FirebaseApp

--- src/FirebaseApp.js
+++ src/FirebaseApp.js
@@ -5,7 +5,9 @@ const firebaseConfig = {
 apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
 authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
 databaseURL: 'https://' + process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseio.com',
-projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
+projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+appId: process.env.VUE_APP_FIREBASE_APP_ID
 }
 
 // Initialize Firebase
