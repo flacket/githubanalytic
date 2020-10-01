d4f320d8c4a19d4cbd5698da18cefe6b4b002029
flacket <facundoggallardo@gmail.com>
2020-10-01 13:53:21 -0300

    add RepoDescarga Page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -69,7 +69,7 @@ export default {
         //{ icon: 'home', text: 'Inicio', route: '/' },
         { icon: "list_alt", text: "Pull Request", route: "/pullrequest" },
         { icon: "timeline", text: "Repositorio", route: "/repositorio" },
-        //{ icon: "person", text: "Participantes", route: "/userstats" },
+        { icon: "mdi-download", text: "RepoDescarga", route: "/repodescarga" },
         { icon: "help", text: "Acerca de", route: "/acerca" },
       ],
     };
--- src/router.js
+++ src/router.js
@@ -1,80 +1,89 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import firebaseApp from './FirebaseApp'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import firebaseApp from "./FirebaseApp";
 
-import Repositorio from './views/Repositorio.vue'
-import UserStats from './views/UserStats.vue'
-import PullRequest from './views/PullRequest.vue'
-import Acerca from './views/Acerca.vue'
+import Repositorio from "./views/Repositorio.vue";
+import UserStats from "./views/UserStats.vue";
+import PullRequest from "./views/PullRequest.vue";
+import Acerca from "./views/Acerca.vue";
+import repoDescarga from "./views/repoDescarga.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 let router = new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '*',
-      redirect: '/'
+      path: "*",
+      redirect: "/",
     },
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/pullrequest',
-      name: 'pullrequest',
+      path: "/pullrequest",
+      name: "pullrequest",
       component: PullRequest,
-      meta:{
-        requiresAuth: true
-      }
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/repositorio',
-      name: 'repositorio',
-      component: Repositorio
+      path: "/repositorio",
+      name: "repositorio",
+      component: Repositorio,
     },
     {
-      path: '/userstats',
-      name: 'userstats',
+      path: "/repodescarga",
+      name: "repodescarga",
+      component: repoDescarga,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/userstats",
+      name: "userstats",
       component: UserStats,
-      meta:{
-        requiresAuth: true
-      }
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/acerca',
-      name: 'acerca',
-      component: Acerca
-    }
-  ]
-})
+      path: "/acerca",
+      name: "acerca",
+      component: Acerca,
+    },
+  ],
+});
 
 //Nav Royal Guards
 router.beforeEach((to, from, next) => {
   //check for requireddAuth guard
-  if(to.matched.some(record => record.meta.requiresAuth)) {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
     //check if NOT logged in
-    if(!firebaseApp.auth().currentUser){
-      //Go to login 
+    if (!firebaseApp.auth().currentUser) {
+      //Go to login
       next({
-        path: '/error',
-        query:{
-          redirect: to.fullPath
-        }
+        path: "/error",
+        query: {
+          redirect: to.fullPath,
+        },
       });
     } else next();
-  } else if (to.matched.some(record => record.meta.requiresGuest)){
-      //check if logged in
-    if(firebaseApp.auth().currentUser){
-      //Go to login 
+  } else if (to.matched.some((record) => record.meta.requiresGuest)) {
+    //check if logged in
+    if (firebaseApp.auth().currentUser) {
+      //Go to login
       next({
-        path: '/',
-        query:{
-          redirect: to.fullPath
-        }
+        path: "/",
+        query: {
+          redirect: to.fullPath,
+        },
       });
     } else next();
   } else next();
